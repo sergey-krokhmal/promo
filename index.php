5b102afb21153aa7615da692c25be8de739f3699
sergey-krokhmal <xai.sergey.krokhmal@gmail.com>
2017-10-18 02:05:15 +0300

    Add svg icon font

--- index.php
+++ index.php
@@ -1,18 +1,25 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="/assets/libs/jquery/jquery-3.2.1.min.js"></script>
         <script src="/assets/libs/jquery/onepage-scroll/jquery.onepage-scroll.min.js"></script>
         <link rel="stylesheet" href="/assets/libs/jquery/onepage-scroll/onepage-scroll.css">
-        <link rel="stylesheet" href="/assets/libs/bootstrap/bootstrap.min.css">
-        <link rel="stylesheet" href="/assets/libs/bootstrap/bootstrap-theme.min.css">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/assets/libs/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/assets/libs/bootstrap-3.3.7-dist/css/bootstrap-theme.min.css">
         <link rel="stylesheet" href="/assets/css/style.css">
+        <link rel="stylesheet" href="/assets/fonts/svg-font/svg-font.css">
+        <link rel="stylesheet" href="/assets/fonts/Circe/stylesheet.css">
     </head>
     <body>
         <div class="main">
             <div class="onepage-scroll-wrapper">
-                <section class="container-fluid main-page">
+                <section class="container main-page">
+                    <div>Check font</div>
+                    <div>Проверка фона</div>
+                    <div>Проверка фона</div>
+                    <div>Проверка фона</div>
+                    <div>Проверка фона</div>
                     <img class="car" src="/assets/images/png/car.png"/>
                 </section>
                 <section class="container-fluid reg-page">2</section>
