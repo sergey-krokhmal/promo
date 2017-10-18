2f6bb9f2529ed2426e127e4852b759279fdb1c7a
sergey-krokhmal <xai.sergey.krokhmal@gmail.com>
2017-10-18 04:35:01 +0300

    finish background parts

--- index.php
+++ index.php
@@ -7,22 +7,32 @@
         <link rel="stylesheet" href="/assets/libs/jquery/onepage-scroll/onepage-scroll.css">
         <link rel="stylesheet" href="/assets/libs/bootstrap-3.3.7-dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="/assets/libs/bootstrap-3.3.7-dist/css/bootstrap-theme.min.css">
-        <link rel="stylesheet" href="/assets/css/style.css">
         <link rel="stylesheet" href="/assets/fonts/svg-font/svg-font.css">
+        <link rel="stylesheet" href="/assets/css/style.css">
         <link rel="stylesheet" href="/assets/fonts/Circe/stylesheet.css">
     </head>
     <body>
-        <div class="main">
+        <div class="main-promo">
+            <span class="bg-part bg-part-1 svg-icon-light"></span>
+            <span class="bg-part bg-part-2 svg-icon-dashboard"></span>
+            <span class="bg-part bg-part-3 svg-icon-map-location"></span>
+            <span class="bg-part bg-part-4 svg-icon-speed-limit"></span>
+            <span class="bg-part bg-part-5 svg-icon-gearbox-2"></span>
+            <span class="bg-part bg-part-6 svg-icon-gearbox"></span>
+            <span class="bg-part bg-part-7 svg-icon-traffic-light"></span>
+            <span class="bg-part bg-part-8 svg-icon-mechanics"></span>
+            <span class="bg-part bg-part-9 svg-icon-steering-wheel"></span>
             <div class="onepage-scroll-wrapper">
-                <section class="container main-page">
-                    <div>Check font</div>
-                    <div>Проверка фона</div>
-                    <div>Проверка фона</div>
-                    <div>Проверка фона</div>
-                    <div>Проверка фона</div>
-                    <img class="car" src="/assets/images/png/car.png"/>
+                <section class="container-fluid main-promo-page">
+                    <div class="promo-content">
+                        <header>
+                            <span class="logo svg-icon-logo-bm"></span>
+                            <span class="gerb-text"><span class="gerb svg-icon-gerb"></span> Первый региональный каршеринг</span>
+                        </header>
+                    </div>
+                    <img class="animated-promo-car" src="/assets/images/png/car.png"/>
                 </section>
-                <section class="container-fluid reg-page">2</section>
+                <section class="container-fluid reg-promo-page">2</section>
             </div>
         </div>
         <script>
